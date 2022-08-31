2b1bb6cbf01e8e5d51ddbd07b45a3580c3d38ed6
hc-5514 <tbd05050@naver.com>
2022-08-31 16:01:47 +0900

    post table ddl 추가

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -12,3 +12,17 @@ create or replace table member
 )
     comment '회원 정보';
 
+create or replace table post
+(
+    id       bigint auto_increment
+        primary key,
+    title    varchar(20)  not null,
+    name     varchar(20)  not null,
+    date     datetime     not null,
+    contents varchar(255) not null,
+    constraint post_member_member_id_fk
+        foreign key (name) references member (member_id)
+)
+    comment '게시글';
+
+
